0740e96ff26d8aa49b1b4f0c5ff60e074a3da6a6
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-10-21 20:20:50 +0700

    stickyHeader

--- app/src/main/java/com/rujirakongsomran/jc_lazycolumncustom/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/jc_lazycolumncustom/MainActivity.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -13,17 +17,43 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.rujirakongsomran.jc_lazycolumncustom.repository.PersonRepository
 import com.rujirakongsomran.jc_lazycolumncustom.ui.theme.JC_LazyColumnCustomTheme
 
 class MainActivity : ComponentActivity() {
+
+
+    @ExperimentalFoundationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             JC_LazyColumnCustomTheme {
-                MyApp()
+                val sections = listOf("A", "B", "C", "D", "E")
+                LazyColumn(
+                    contentPadding = PaddingValues(all = 12.dp),
+                    verticalArrangement = Arrangement.spacedBy(12.dp)
+                ) {
+                    sections.forEach { section ->
+                        stickyHeader {
+                            Text(
+                                text = "Section $section",
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .background(MaterialTheme.colors.secondary)
+                                    .padding(12.dp)
+                            )
+                        }
+                        items(10) {
+                            Text(
+                                text = "Item $it from the section $section",
+                                modifier = Modifier.padding(12.dp)
+                            )
+                        }
+                    }
+                }
             }
         }
     }
