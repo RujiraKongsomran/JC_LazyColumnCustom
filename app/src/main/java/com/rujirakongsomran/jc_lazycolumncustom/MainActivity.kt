29342f35cba2325865d30fc8a50645dbf9eb129c
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-10-21 10:39:25 +0700

    Display data in LazyColumn

--- app/src/main/java/com/rujirakongsomran/jc_lazycolumncustom/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/jc_lazycolumncustom/MainActivity.kt
@@ -3,11 +3,14 @@ package com.rujirakongsomran.jc_lazycolumncustom
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.rujirakongsomran.jc_lazycolumncustom.repository.PersonRepository
 import com.rujirakongsomran.jc_lazycolumncustom.ui.theme.JC_LazyColumnCustomTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,26 +18,35 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JC_LazyColumnCustomTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                MyApp()
             }
         }
     }
-}
 
+    @Composable
+    fun MyApp() {
+        Surface(color = MaterialTheme.colors.background) {
+            DisplayData()
+        }
+    }
 
+    @Composable
+    fun DisplayData() {
+        val personRepository = PersonRepository()
+        val getAllData = personRepository.getAllData()
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+        LazyColumn() {
+            items(items = getAllData) { person ->
+                CustomItem(person = person)
+            }
+        }
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    JC_LazyColumnCustomTheme {
-        Greeting("Android")
+    @Preview(showBackground = true)
+    @Composable
+    fun PreviewScreen() {
+        JC_LazyColumnCustomTheme {
+            MyApp()
+        }
     }
 }
